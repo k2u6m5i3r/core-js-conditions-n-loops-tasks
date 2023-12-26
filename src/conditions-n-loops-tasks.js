a47ce425d8b760902fd49f54f31c8576dc7c2af1
Sergei Shevchenko <wunder49@yandex.by>
2023-12-26 21:36:04 +0300

    add solution for getMaxNumber

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -38,8 +38,17 @@ function isPositive(number) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  let max = null;
+  if (a > b && a > c) {
+    max = a;
+  } else if (b > a && b > c) {
+    max = b;
+  } else {
+    max = c;
+  }
+
+  return max;
 }
 
 /**
